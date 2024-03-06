ee893c134a938362512035d44ce68aa8eece9aab
codingwithjava <sr7363781@gmail.com>
2024-03-06 14:22:35 +0530

    add

---
+++ src/main/java/com/tom/TomApplication.java
@@ -0,0 +1,21 @@
+package com.tom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomApplication.class, args);
+	}
+	@RequestMapping(value = "/")
+	public String helloWorld()
+	{
+		return "hello";
+	}
+
+}
